b21c7da474a77dcd16d566d1d07a6400def10e9e
Anujshivhare <itachiuchihaas2001@gmail.com>
2024-05-20 14:20:09 +0530

    Completed

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,40 @@
+import { useState, useCallback, useRef } from "react";
+import QUESTIONS from "../questions.js";
+import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswer] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const isQuizComplete = QUESTIONS.length === userAnswers.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswer((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (isQuizComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      ></Question>
+    </div>
+  );
+}
